2b5e74622ac9b543e4bb3723af762e2362b7821c
Rania1852005 <ranibenamara55@gmail.com>
2026-04-17 01:21:48 +0100

    Update and rename enseignant/dashboard.php to Mes_modules.php
    modification

--- Mes_modules.php
+++ Mes_modules.php
@@ -61,11 +61,10 @@
       <table id="modulesTable">
         <thead><tr><th>Code</th><th>Intitulé</th><th>Coefficient</th><th>Crédit</th><th>Type</th><th>Statut</th></tr></thead>
         <tbody>
-          <tr><td>INF101</td><td class="td-name">Introduction à la programmation</td><td>3</td><td>5</td><td>Cours + TP</td><td><span class="badge-done">Done</span></td></tr>
-          <tr><td>INF102</td><td class="td-name">Algorithmique</td><td>4</td><td>6</td><td>Cours + TD</td><td><span class="badge-done">Done</span></td></tr>
-          <tr><td>INF103</td><td class="td-name">Base de données</td><td>3</td><td>5</td><td>Cours + TP</td><td><span class="badge-done">Done</span></td></tr>
-          <tr><td>INF104</td><td class="td-name">Programmation Web</td><td>3</td><td>5</td><td>Cours + TP</td><td><span class="badge-done">Done</span></td></tr>
-          <tr><td>INF105</td><td class="td-name">Systèmes d'exploitation</td><td>4</td><td>6</td><td>Cours + TD</td><td><span class="badge-done">Done</span></td></tr>
+          <tr><td>INF101</td><td class="td-name">Algorithmique</td><td>4</td><td>6</td><td>Cours + TD + TP</td><td><span class="badge-done">Done</span></td></tr>
+          <tr><td>INF102</td><td class="td-name">Base de données</td><td>3</td><td>5</td><td>Cours + TP</td><td><span class="badge-done">Done</span></td></tr>
+          <tr><td>INF103</td><td class="td-name">Programmation Web</td><td>3</td><td>5</td><td>Cours + TP</td><td><span class="badge-done">Done</span></td></tr>
+          <tr><td>INF104</td><td class="td-name">Systèmes d'exploitation</td><td>4</td><td>6</td><td>Cours + TD</td><td><span class="badge-done">Done</span></td></tr>
         </tbody>
       </table>
       <div class="pagination">
